2134c0c605ad0601c229a502b213dc0518949bf6
ys93239 <yash.shrivastav25@uga.edu>
2016-10-05 11:57:41 -0400

    Tuning the Hyperparameters
    Added all the optimizers to check the accuracy against each other.

--- keras_small.py
+++ keras_small.py
@@ -8,7 +8,17 @@ from keras.layers import Flatten
 from keras.layers import Dense
 from keras.constraints import maxnorm
 from keras.optimizers import SGD
+from keras.optimizers import RMSprop
+from keras.optimizers import Adagrad
+from keras.optimizers import Adadelta
+from keras.optimizers import Adam
+from keras.optimizers import Adamax
+from keras.optimizers import Nadam
+
 from keras.utils import np_utils
+
+np.random.seed(7)
+
 
 # train image data read and conversion to ndarray of dimensions (no. of images, width, height, channels)
 
@@ -20,7 +30,7 @@ def create_pd_input(path):
 
     pixel_data = []
     for name in image_files_names:
-        pixel_data.append(imread('./data/images/' + name + '.png', mode='RGB'))
+        pixel_data.append(imread('./data/images/image_data/' + name + '.png', mode='RGB'))
     pixel_data = np.asarray(pixel_data)
     return pixel_data.astype('float32') / 255.0
 
@@ -48,24 +58,34 @@ label_test_ohe = process_labels('./data/y_small_test.txt')
 
 def create_model():
     cnn_model = Sequential()
-    cnn_model.add(Convolution2D(32, 3, 3, activation='relu', dim_ordering='tf', border_mode='same', input_shape=(32, 32, 3), W_constraint=maxnorm(3), bias=True))
+    cnn_model.add(Convolution2D(32, 3, 3, activation='relu', dim_ordering='tf', border_mode='same', input_shape=(32, 32, 3), W_constraint=maxnorm(3)))
     cnn_model.add(Dropout(0.2))
-    cnn_model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same', W_constraint=maxnorm(3), bias=True))
+    cnn_model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same', W_constraint=maxnorm(3)))
     cnn_model.add(MaxPooling2D(pool_size=(2, 2)))
     cnn_model.add(Flatten())
     cnn_model.add(Dense(512, activation='relu',W_constraint=maxnorm(3)))
     cnn_model.add(Dropout(0.5))
     cnn_model.add(Dense(10, activation='softmax'))
-    
-    sgd = SGD(lr=0.01, momentum=0.9, decay=(0.01/10), nesterov=False)
-    cnn_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    #sgd = SGD(lr=0.01)
+    #sgd = SGD(lr=0.01, momentum=0.9, decay=(0.01/10), nesterov=False)
+    #rms=RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+    #ad=Adagrad(lr=0.01, epsilon=1e-08, decay=0.0)
+    #adadelta = Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+    #adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    #amax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    nadam = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
+
+    cnn_model.compile(loss='categorical_crossentropy', optimizer= nadam, metrics=['accuracy'])
     return cnn_model
 
 def main():
     model = create_model()
-    model.fit(X_pd_train, label_train_ohe, validation_data=(X_pd_test, label_test_ohe), nb_epoch=100,          batch_size=10, verbose=2)
+    model.fit(X_pd_train, label_train_ohe, nb_epoch=10,batch_size=10, verbose=2)
+    #, validation_data=(X_pd_train, label_train_ohe)
+    
+    
     acc = model.evaluate(X_pd_test, label_test_ohe, verbose=0)
     print 'accuracy %.2f' % (acc[1]*100)
-
+    
 if __name__ == '__main__':
     main()
